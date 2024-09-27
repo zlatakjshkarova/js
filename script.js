3da4842aec04e6f9f99195bb090fc94b9b33126b
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-09-27 22:16:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+
+let x = (5);
+x = String(x);
+console.log(x);
+
+let y = ('hello');
+y = String(y);
+console.log(y);
+
+let z = (true);
+z = String(z);
+console.log(z);
+
+
+let w = null;
+w = String(w);
+console.log(w);
+
+
+
+
+
+
+let r = ('10');
+r = Number(r);
+console.log(r);
+
+
+let t = ("20.5");
+t = Number(t);
+console.log(t);
+
+let f = ("30px");
+f = Symbol(f);
+console.log(f)
+
+
+
+let h = ('10.5');
+h = Number(h);
+console.log(h);
+
+
+let q = ('20');
+q = Number(q);
+console.log(q);
+
+let b = ("20.35px");
+b = Symbol(b);
+console.log(b);
+
+let userName = ('Джон');
+let age = 30;
+console.log(`Меня зовут ${userName} ,мне ${age} лет  `);
+
+let ilke = ("Я изучаю JavaScript");
+ilke = alert(ilke);
+console.log(ilke);
+
+
+let num = (123);
+num = alert(num);
+console.log(num);
+
+let a = (1 + 2 + 3);
+a = Number(a);
+a = alert(a);
+console.log(a);
+
+
+
+let c = (3);
+let c2 = (1);
+c2 = Number(c2)
+
+c = Numbec2 = prompt(c * c2)
+c, c2 = prompt(c, c2);
+
+console.log(c - c2, c * c2, c / c2);
